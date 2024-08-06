92359418e236504917502bbdc4734d68165fe797
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-08-07 03:08:06 +0500

    Update apis.js

--- apis.js
+++ apis.js
@@ -1,7 +1,14 @@
-// Get TimeZone
-var endpoint = "http://ip-api.com/json/?fields=status,message,countryCode";
+// An example script for redirecting users from USA to https://google.com/
+// and users from Canada to https://google.ca/
+
+// ip-api endpoint URL
+// we need only the countryCode, but you can request more fields
+// see http://ip-api.com/docs/api:json for documentation
+var endpoint =
+  "http://ip-api.com/json/?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,query";
 
 var xhr = new XMLHttpRequest();
+
 xhr.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var response = JSON.parse(this.responseText);
@@ -11,12 +18,16 @@ xhr.onreadystatechange = function () {
     }
     // Redirect
     if (response.countryCode == "PK") {
-      window.location.replace("https://google.com/");
+      //   window.location.replace("https://google.com/");
+      return response;
     }
     if (response.countryCode == "CA") {
       window.location.replace("https://google.ca/");
     }
   }
 };
+
 xhr.open("GET", endpoint, true);
 xhr.send();
+
+console.log();

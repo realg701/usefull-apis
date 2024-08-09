154cd781d846cd06a7aa2b3ffed1df9df4ba1686
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-08-10 00:47:39 +0500

    removed

---
+++ main.js
@@ -0,0 +1,31 @@
+// An example script for redirecting users from USA to https://google.com/
+// and users from Canada to https://google.ca/
+
+// ip-api endpoint URL
+// we need only the countryCode, but you can request more fields
+// see http://ip-api.com/docs/api:json for documentation
+const endpoint =
+  "http://ip-api.com/json/?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,query";
+
+const xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var response = JSON.parse(this.responseText);
+    if (response.status !== "success") {
+      console.log("query failed: " + response.message);
+      return;
+    }
+    // Redirect
+    if (response.countryCode == "PK") {
+      window.location.replace("https://google.com/");
+      return response;
+    }
+    if (response.countryCode == "CA") {
+      window.location.replace("https://google.ca/");
+    }
+  }
+};
+
+xhr.open("GET", endpoint, true);
+xhr.send();
